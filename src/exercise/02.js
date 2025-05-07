79e7a620791842fa796d5a674048533ec56e5059
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-05-07 17:28:35 +0200

    Exercice 02 Terminée

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,16 +3,22 @@
 // http://localhost:3000/alone/exercise/02.js
 
 // eslint-disable-next-line no-unused-vars
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init('JavaScript - Template Literals')
+  init("JavaScript - Template Literals")
 
-// 🐶 Créé 3 constantes avec leurs valeurs 5 10 et 200
+  // 🐶 Créé 3 constantes avec leurs valeurs 5 10 et 200
+  const five = 5
+  const ten = 10
+  const twoHundred = 200
 
-// 🐶 utilise la fonction `Math.max` pour trouver le nombre le plus grand
-// 📑 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+  let lib = `Le nombre le plus grand entre ${five} ${ten} ${twoHundred} est ${Math.max(five, ten, twoHundred)} !`
+  displayText(lib)
 
-// 🐶 affiche le liblélé “Le nombre le plus grand entre 5 10 200 est 200 !”
-// à l'écran avec 'displayText'
-};
-export default () => <App exercice={exercice} />;
+  // 🐶 utilise la fonction `Math.max` pour trouver le nombre le plus grand
+  // 📑 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+
+  // 🐶 affiche le liblélé “Le nombre le plus grand entre 5 10 200 est 200 !”
+  // à l'écran avec 'displayText'
+}
+export default () => <App exercice={exercice} />
