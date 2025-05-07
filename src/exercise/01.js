c335838063dee8a5ff0af623feaf026bd4c2d91e
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-05-07 15:02:43 +0200

    Exercice 01 Terminée

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,23 +1,37 @@
 "use client"
+
 // ESModule Import / Export
 // http://localhost:3000/alone/exercise/01.js
-import { App } from "../lib/exerciseHelper"
+import {
+  calculAir,
+  calculPerimetre,
+  TEMP_MAX,
+} from "../exercise/helper/roomHelper"
+import displayText, { App, init } from "../lib/exerciseHelper"
 // 🐶 Importe 'init' et 'displayText' du module qui permet d'afficher un texte dans la page de l'exercice
 //  🤖 import displayText, { init } from "../lib/exerciseHelper";
 const exercice = () => {
-// 🐶 Affiche 'HelloWorld' dans la page grace à la fonction `displayText` importée
-// Atention il faut appeler une fois la fonction init avant les appels à displayText
-//  🤖 init()
-//  🤖 displayText("Hello World")
+  // 🐶 Affiche 'HelloWorld' dans la page grace à la fonction `displayText` importée
+  // Atention il faut appeler une fois la fonction init avant les appels à displayText
+  //  🤖 init()
+  //  🤖 displayText("Hello World")
+  init()
+  displayText("Hello Word")
 
-// 🐶 Pour la suite de l"exercice il faut aller renseigner le module dans
-// `src/helper/roomHelper.js`
+  // 🐶 Pour la suite de l"exercice il faut aller renseigner le module dansS
+  // `src/helper/roomHelper.js`
 
-// 🐶 Importe tout du module roomHelper
+  // 🐶 Importe tout du module roomHelper
 
-// 🐶 Utilise ces 3 fonctions pour créér les bons libéllés
-// 🤖 let lib = "Air de d'un pièce de 2 par 5 donne " + ...
-// Utilise displayText pour afficher ces 3 libéllés
+  // 🐶 Utilise ces 3 fonctions pour créér les bons libéllés
+  // 🤖 let lib = "Air de d'un pièce de 2 par 5 donne " + ...
+  // Utilise displayText pour afficher ces 3 libéllés
 
-};
-export default () => <App exercice={exercice} />;
+  let lib = `Air de d'un pièce de 2 par 5 donne ${calculAir(2, 5)}`
+  displayText(lib)
+  lib = `Périmètre de d'un pièce de 2 par 5 donne ${calculPerimetre(2, 5)}`
+  displayText(lib)
+  lib = `La température max d'une pièce est ${TEMP_MAX}`
+  displayText(lib)
+}
+export default () => <App exercice={exercice} />
--- src/exercise/helper/roomHelper.js
+++ src/exercise/helper/roomHelper.js
@@ -1,8 +1,16 @@
 // 🐶 créé une fonction `calculAir` (air = longeur x largeur) qui sera exportée par defaut
 // 🤖 export default function
 
+export const calculAir = (longeur, largeur) => {
+  return longeur * largeur
+}
+
 // 🐶 créé une fonction `calculPerimetre` (perimètre = longeur x largeur x 2) qui sera exportée
 // 🤖 export function
+export const calculPerimetre = (longeur, largeur) => {
+  return (longeur + largeur) * 2
+}
 
 // 🐶 créé une constante `TEMP_MAX` = 60 qui sera exportée
 // 🤖 export const
+export const TEMP_MAX = 60
